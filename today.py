b99dbefba8b1866a97e814882f20a96693ecfbbf
busra akyuz <busramaralakyuz@gmail.com>
2026-04-21 13:34:28 +0300

    Add docstrings to today functions

--- today.py
+++ today.py
@@ -2,4 +2,11 @@ def my_name_is():
     return "Büşra"
 def my_buddy_is():
     return "Ahmet"
-        
+def my_name_is():
+    """Return my name."""
+    return "Büşra"
+
+
+def my_buddy_is():
+    """Return my buddy's name."""
+    return "Ahmet"        
