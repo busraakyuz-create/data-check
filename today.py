7eda36f2eade215c80aed1bd96df147008515bca
busra akyuz <busramaralakyuz@gmail.com>
2026-04-21 13:35:08 +0300

    Fix duplicate functions

--- today.py
+++ today.py
@@ -1,12 +1,8 @@
 def my_name_is():
-    return "Büşra"
-def my_buddy_is():
-    return "Ahmet"
-def my_name_is():
     """Return my name."""
     return "Büşra"
 
 
 def my_buddy_is():
     """Return my buddy's name."""
-    return "Ahmet"        
+    return "Ahmet"
